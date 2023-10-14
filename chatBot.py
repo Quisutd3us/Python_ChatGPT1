0b231b504f81e82fc927dca080baa76595f646e9
d3us <dnarino@gmail.com>
2023-10-14 15:55:08 -0500

    add confiability modele tochatbot

--- chatBot.py
+++ chatBot.py
@@ -2,6 +2,7 @@ import os
 from dotenv import load_dotenv
 import openai
 import spacy
+import numpy as np
 
 load_dotenv()
 api_key = os.getenv('OPENAI_API_KEY')
@@ -12,6 +13,21 @@ black_list = ["sex", "nudes"]
 
 history_answers = []
 history_questions = []
+
+
+def similarity_cos(vec1, vec2):
+    superposition = np.dot(vec1, vec2)
+    magnitude_1 = np.linalg.norm(vec1)
+    magnitude_2 = np.linalg.norm(vec2)
+    sim_cos = superposition / (magnitude_1 * magnitude_2)
+    return sim_cos
+
+
+def is_relevant(fn_response, fn_input, umbral=0.5):
+    vector_input = model_spacy(fn_input).vector
+    vector_response = model_spacy(fn_response).vector
+    simil = similarity_cos(vector_input, vector_response)
+    return simil >= umbral
 
 
 def filter_black_list(fn_response_ai, fn_black_list):
@@ -52,7 +68,10 @@ while True:
     prompt = f"The user Ask: {user_input}\n"
     format_history_conversation += prompt
     response = ask_chat_gpt(format_history_conversation)
-    print(f"{response}")
 
-    history_questions.append(user_input)
-    history_answers.append(response)
+    if is_relevant:
+        print(f"{response}")
+        history_questions.append(user_input)
+        history_answers.append(response)
+    else:
+        print(f"The response is not relevant")
