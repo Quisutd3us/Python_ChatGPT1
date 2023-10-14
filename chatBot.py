59a0148d90c1d54b3f7f86060b90d75f6b95b6ce
d3us <dnarino@gmail.com>
2023-10-14 12:00:49 -0500

    add black_list filter function to chatBot.py

--- chatBot.py
+++ chatBot.py
@@ -1,13 +1,28 @@
 import os
 from dotenv import load_dotenv
 import openai
+import spacy
 
 load_dotenv()
 api_key = os.getenv('OPENAI_API_KEY')
 openai.api_key = api_key
 
+model_spacy = spacy.load("es_core_news_md")
+black_list = ["sex", "nudes"]
+
 history_answers = []
 history_questions = []
+
+
+def filter_black_list(fn_response_ai, fn_black_list):
+    token = model_spacy(fn_response_ai)
+    filter_result = []
+    for t in token:
+        if t.text.lower() not in fn_black_list:
+            filter_result.append(t.text)
+        else:
+            filter_result.append("['PROHIBITED-WORD']")
+    return " ".join(filter_result)
 
 
 def ask_chat_gpt(fn_prompt, model="gpt-3.5-turbo-instruct"):
@@ -21,7 +36,7 @@ def ask_chat_gpt(fn_prompt, model="gpt-3.5-turbo-instruct"):
         # number of characters in response
         max_tokens=150
     )
-    return response_ai.choices[0].text.strip()
+    return filter_black_list(response_ai.choices[0].text.strip(), black_list)
 
 
 print('Welcome to Our ChatBot, Write EXIT when you will Finish...')
--- checkFeelings.py
+++ checkFeelings.py
@@ -26,4 +26,31 @@ def do_analyze_feelings(fn_engine, fn_text, fn_n, fn_temperature, fn_max_tokens)
     return response_ai.choices[0].text.strip()
 
 
-print(f"{do_analyze_feelings('gpt-3.5-turbo-instruct', data, 1, 1.5, 500)}")
+def do_categorize_text(fn_engine, fn_text, fn_n, fn_temperature, fn_max_tokens):
+    categories = [
+        'art',
+        'science',
+        'sports',
+        'economy',
+        'education',
+        'entertainment',
+        'ecology',
+        'politics',
+        'health',
+        'tech'
+    ]
+    fn_prompt = f"Please categorize the following text: {fn_text} into a category of the following: {','.join(categories)}"
+    print(fn_prompt)
+    response_ai = openai.Completion.create(
+        engine=fn_engine,
+        prompt=fn_prompt,
+        n=fn_n,
+        temperature=fn_temperature,
+        max_tokens=fn_max_tokens
+    )
+    return response_ai.choices[0].text.strip()
+
+
+print(f"Result feeling Analysis : \n {do_analyze_feelings('gpt-3.5-turbo-instruct', data, 1, 0.5, 20)}")
+
+print(f"The Category is: \n {do_categorize_text('gpt-3.5-turbo-instruct', data, 1, 0.5, 20)}")
